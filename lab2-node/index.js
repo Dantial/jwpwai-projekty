4321c9e0f1925b3e661abcb0a6dc103c956589d9
Dantial <danielm001@o2.pl>
2025-10-17 22:53:30 +0200

    Kompletne laboratorium nr 2

---
+++ lab2-node/index.js
@@ -0,0 +1,59 @@
+// Zadanie 1.
+const wait = 3000;
+console.log("Początek skryptu...");
+setTimeout(function () {
+  console.log("Upłynęły minimum 3 sek...");
+}, wait);
+console.log("koniec skryptu..."); 
+
+// Zadanie 2.
+const fs = require("fs");
+const data = fs.readFileSync("package.json");
+console.log("Początek skryptu.");
+console.log(data.toString());
+console.log("Koniec skryptu."); 
+
+const fs = require("fs");
+console.log("Początek skryptu.");
+fs.readFile("package.json", function (err, data) {
+  if (err) return console.error(err);
+  console.log(data.toString());
+});
+console.log("Koniec skryptu."); 
+
+// Zadanie 3.
+const http = require("http");
+const hostname = "127.0.0.1";
+const port = 8888;
+const server = http.createServer(function (request, response) {
+  response.statusCode = 200;
+  response.setHeader("Content-Type", "text/plain");
+  response.end("Zmieniony tekst na serwerze node.js !\n");
+});
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+}); */
+
+const http = require("http");
+const path = require("path");
+const util = require("util");
+const v8 = require("v8");
+const hostname = "127.0.0.1";
+const port = 8888;
+const server = http.createServer((request, response) => {
+  response.statusCode = 200;
+  response.setHeader("Content-Type", "text/plain");
+  response.end("Hello, from node.js serwer !\n");
+});
+server.listen(port, hostname, () => {
+  console.log(v8.getHeapStatistics());
+  console.log(path.basename(__filename));
+  console.log(path.join(__dirname, "uploads", "images"));
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+// Zadanie 4.
+import uniqueString from "unique-string";
+
+console.log(uniqueString());
+console.log("Zaimportowany moduł zwraca unikalny 32-znakowy łańcuch.");
