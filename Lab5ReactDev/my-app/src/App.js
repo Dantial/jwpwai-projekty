efbddf71bd095a58619697d9610d23dd6cbea70c
Dantial <danielm001@o2.pl>
2026-01-15 19:13:13 +0100

    ciąg dalszy laboratorium

--- Lab5ReactDev/my-app/src/App.js
+++ Lab5ReactDev/my-app/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import Hello from "./Hello";
+import Counter from "./Counter";
+import InputTracker from "./InputTracker";
+import LoginStatus from "./LoginStatus";
+import TodoList from "./TodoList";
+import LoginForm from "./LoginForm";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Hello name="Anna" />
+      <Hello name="Bartek" />
+      <Hello name="Celina" />
+      <Counter />
+      <InputTracker />
+      <LoginStatus isLoggedIn={true} />
+      <LoginStatus isLoggedIn={false} />
+      <TodoList todos={["Zrobić trening", "Zrobić zakupy", "Posprzątać auto"]} />
+      <LoginForm />
     </div>
   );
 }
-
 export default App;
--- Lab5ReactDev/my-app/src/Hello.js
+++ Lab5ReactDev/my-app/src/Hello.js
@@ -1,23 +1,16 @@
 // Ćwiczenie 1.
 
-/* function Hello() {
- return <h1>Daniel Monkiewicz 20514</h1>;
+/*
+function Hello() {
+  return <h1>Daniel Monkiewicz 20514</h1>;
 }
-export default Hello; */
+
+*/
 
 // Ćwiczenie 2.
 
-function HelloWithProps(props) {
- return <h1>Witaj, {props.name}!</h1>;
+function Hello(props) {
+  return <h1>Witaj, {props.name}!</h1>;
 }
 
-function App() {
- return (
- <div>
- <HelloWithProps name="Anna" />
- <HelloWithProps name="Bartek" />
- <HelloWithProps name="Celina" />
- </div>
- );
-}
-export default App;
+export default Hello;
